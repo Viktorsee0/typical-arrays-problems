a18dd9d29e69695383a1eee01ce39a6dfcdbe5c1
Viktor <viktorsee0@gmail.com>
2020-09-30 16:33:08 +0300

    docs: add min, max, avg func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return arguments.length > 0 ? array.reduce(function (min, current) {
+        return (min < current ? min : current)
+    }, 0) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return arguments.length > 0 ? array.reduce(function (max, current) {
+        return (max > current ? max : current)
+    }, 0) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return arguments.length > 0 ? array.length > 0 ? array.reduce((avg, current) =>
+        (avg + current)) / array.length : 0 : 0;
 }
